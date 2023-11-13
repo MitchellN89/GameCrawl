9a66c35da2808cd50489e9c2615b3754eb954a3a
MitchellN89 <mitchell.nilsson@gmail.com>
2023-11-13 17:09:46 +1300

    Fixing a file

--- GameCrawl/src/context/AllGamesProvider.jsx
+++ GameCrawl/src/context/AllGamesProvider.jsx
@@ -7,7 +7,7 @@ export function AllGamesProvider({ children }) {
 
   useEffect(() => {
     let ignore = false;
-    fetch("https://ree-to-play-games-database.p.rapidapi.com/api/games", {
+    fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": "32b9ac705fmshfe912da10fd865cp1b780bjsn8a6c8659eee3",
